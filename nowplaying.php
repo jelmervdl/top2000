9987644c6db182a261090cc2589da7af103a0dba
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2013-12-26 10:57:52 +0100

    Update for 2013 list

--- nowplaying.php
+++ nowplaying.php
@@ -6,7 +6,14 @@ date_default_timezone_set('Europe/Amsterdam');
 ini_set('display_errors', false);
 error_reporting(E_ALL);
 
-$fhandle = fopen('http://top2012.radio2.nl/data/cache/json/nowplaying.json?_' . time(), 'r');
+$now_playing_data = file_get_contents('http://top2012.radio2.nl/data/cache/json/nowplaying.json?_' . time(), 'r');
 
-if ($fhandle)
-	fpassthru($fhandle);
+$site = file_get_contents('2013.html');
+
+$now_playing = json_decode($now_playing_data);
+
+if (preg_match('{<u data-prev="\d+" data-daletid="' . preg_quote($now_playing->dalet_id) . '">(\d+)</u>}', $site, $match))
+	$now_playing->position = $match[1];
+
+echo json_encode($now_playing);
+
