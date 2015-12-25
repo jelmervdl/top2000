d6a3af4183e9d90203c08881547f46c17e5d4c56
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-12-25 10:56:25 +0100

    Import changes of 2014 and update for 2015.

--- lyrics.php
+++ lyrics.php
@@ -1,12 +1,28 @@
 <?php
-header('Content-Type: application/json');
-
 date_default_timezone_set('Europe/Amsterdam');
 
-ini_set('display_errors', false);
+ini_set('display_errors', true);
 error_reporting(E_ALL);
 
-$fhandle = fopen('http://lystener.com/lyrics/'.urlencode($_GET['artist']).'/'.urlencode($_GET['song']), 'r');
+function decode_entities($text) {
+	return preg_replace_callback("/(&#[0-9]+;)/", function($m) { return mb_convert_encoding($m[1], "UTF-8", "HTML-ENTITIES"); }, $text);
+}
 
-if ($fhandle)
-	fpassthru($fhandle);
+$api_response = file_get_contents('http://lyrics.wikia.com/api.php?fmt=json&action=lyrics&artist='.rawurlencode($_GET['artist']).'&song='.rawurlencode($_GET['song']));
+
+if (preg_match("/'lyrics':'Not found'/", $api_response))
+	$response['error'] = 'Not found';
+
+else if (!preg_match("/'url':'([^']+)'/", $api_response, $match))
+	$response['error'] = 'Could not find url';
+
+else if (($webpage = file_get_contents($match[1])) === false)
+	$response['error'] = 'Could not fetch lyrics';
+
+else if (preg_match('/((?:&#\d+;|<br \\/>){10,})/', $webpage, $match))
+	$response['lyrics'] = decode_entities($match[1]);
+else
+	$response['error'] = 'Could not find lyrics';
+
+header('Content-Type: application/json');
+echo json_encode($response);
--- nowplaying.php
+++ nowplaying.php
@@ -1,19 +1,21 @@
 <?php
-header('Content-Type: application/json');
-
 date_default_timezone_set('Europe/Amsterdam');
 
-ini_set('display_errors', false);
+ini_set('display_errors', true);
 error_reporting(E_ALL);
 
 $now_playing_data = file_get_contents('http://top2012.radio2.nl/data/cache/json/nowplaying.json?_' . time(), 'r');
 
-$site = file_get_contents('2013.html');
+$list = json_decode(file_get_contents('2015.json'));
 
 $now_playing = json_decode($now_playing_data);
 
-if (preg_match('{<u data-prev="\d+" data-daletid="' . preg_quote($now_playing->dalet_id) . '">(\d+)</u>}', $site, $match))
-	$now_playing->position = $match[1];
+foreach ($list as $position => $song) {
+	if ($song->aid == $now_playing->songversion->id) {
+		$now_playing->position = $song->pos;
+		break;
+	}
+}
 
+header('Content-Type: application/json');
 echo json_encode($now_playing);
-
