6f8def6a7a255d8ff0c5b34f32415a7d946b82e6
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-12-26 10:53:41 +0100

    Blurry background, and use the new API for images and current song information.

--- nowplaying.php
+++ nowplaying.php
@@ -4,21 +4,32 @@ date_default_timezone_set('Europe/Amsterdam');
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
-$now_playing_data = file_get_contents('http://top2012.radio2.nl/data/cache/json/nowplaying.json?_' . time(), 'r');
+$now_playing_data = file_get_contents('http://radiobox2.omroep.nl/data/radiobox2/nowonair/2.json?npo_cc_skip_wall=1');
 
-$now_playing = json_decode($now_playing_data);
+$now_playing = json_decode($now_playing_data)->results[0];
 
-if (isset($now_playing->songversion, $now_playing->songversion->id))
+$response = [
+	'artist' => $now_playing->songfile->artist,
+	'title' => $now_playing->songfile->title,
+	'expires' => strtotime($now_playing->stopdatetime)
+];
+
+if (isset($now_playing->songfile, $now_playing->songfile->songversion))
 {
 	$list = json_decode(file_get_contents('2015.json'));
 
 	foreach ($list as $position => $song) {
-		if ($song->aid == $now_playing->songversion->id) {
-			$now_playing->position = $song->pos;
+		if ($song->aid == $now_playing->songfile->songversion->id)
+		{
+			// Prefer the data from the listing as that one is(?) hand-curated
+			$response['title'] = $song->s;
+			$response['artist'] = $song->a;
+			$response['position'] = $song->pos;
+			$response['image'] = sprintf('http://radiobox2.omroep.nl/image/file/%d/title.jpg', $song->img);
 			break;
 		}
 	}
 }
 
 header('Content-Type: application/json');
-echo json_encode($now_playing);
+echo json_encode($response);
