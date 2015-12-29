d905e1cc425c42c09418b84297f5d9fb3d0b1f1a
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-12-29 11:32:28 +0100

    Radio2 seems to mess up their matching quite often. So let's do it ourselves :)

--- nowplaying.php
+++ nowplaying.php
@@ -14,22 +14,45 @@ $response = [
 	'expires' => strtotime($now_playing->stopdatetime)
 ];
 
-if (isset($now_playing->songfile, $now_playing->songfile->songversion))
+function find_song(Callable $compare)
 {
 	$list = json_decode(file_get_contents('2015.json'));
 
-	foreach ($list as $position => $song) {
-		if ($song->aid == $now_playing->songfile->songversion->id)
-		{
-			// Prefer the data from the listing as that one is(?) hand-curated
-			$response['title'] = $song->s;
-			$response['artist'] = $song->a;
-			$response['position'] = $song->pos;
-			$response['image'] = sprintf('http://radiobox2.omroep.nl/image/file/%d/title.jpg', $song->img);
-			break;
-		}
-	}
+	foreach ($list as $position => $song)
+		if ($compare($song))
+			return [
+				'title' => $song->s,
+				'artist' => $song->a,
+				'position' => $song->pos,
+				'image' => sprintf('http://radiobox2.omroep.nl/image/file/%d/title.jpg', $song->img),
+			];
+	
+	return null;
 }
+
+function simplify($text)
+{
+	return preg_replace('/[^a-z0-9]/', '', strtolower($text));
+}
+
+// If it is already linked by songfile, use that information
+if (isset($now_playing->songfile, $now_playing->songfile->songversion)) {
+	$found = find_song(function($song) use ($now_playing) {
+		return $song->aid == $now_playing->songfile->songversion->id;
+	});
+}
+// But sometimes Radio2 messed up, and we are just going try to match it on artist and title
+else {
+	$simplified_artist = simplify($now_playing->songfile->artist);
+	$simplified_title = simplify($now_playing->songfile->title);
+	$found = find_song(function($song) use ($simplified_artist, $simplified_title) {
+		return simplify($song->s) == $simplified_title
+			&& simplify($song->a) == $simplified_artist;
+	});
+}
+
+if ($found)
+	$response = array_merge($response, $found);	
 
 header('Content-Type: application/json');
 echo json_encode($response);
