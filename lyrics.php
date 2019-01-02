29a954249dae82523000db233a65dfd5e3454dad
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2019-01-02 14:45:44 +0100

    Replace file_get_contents with curl to allow for wonky redirects?

--- lyrics.php
+++ lyrics.php
@@ -4,11 +4,22 @@ date_default_timezone_set('Europe/Amsterdam');
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
+function curl_get_contents($url) {
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($curl, CURLOPT_HEADER, 0);
+	$out = curl_exec($curl);
+	curl_close($curl);
+	return $out;
+}
+
 function decode_entities($text) {
 	return preg_replace_callback("/(&#[0-9]+;)/", function($m) { return mb_convert_encoding($m[1], "UTF-8", "HTML-ENTITIES"); }, $text);
 }
 
-$api_response = file_get_contents('http://lyrics.wikia.com/api.php?fmt=json&action=lyrics&artist='.rawurlencode($_GET['artist']).'&song='.rawurlencode($_GET['song']));
+$api_response = curl_get_contents('http://lyrics.wikia.com/api.php?fmt=json&action=lyrics&artist='.rawurlencode($_GET['artist']).'&song='.rawurlencode($_GET['song']));
+
 
 if (preg_match("/'lyrics':'Not found'/", $api_response))
 	$response['error'] = 'Not found';
@@ -16,7 +27,7 @@ if (preg_match("/'lyrics':'Not found'/", $api_response))
 else if (!preg_match("/'url':'(.+?)(?<!\\\\)(?:\\\\{2})*'/", $api_response, $match))
 	$response['error'] = 'Could not find url';
 
-else if (($webpage = file_get_contents($match[1])) === false)
+else if (($webpage = curl_get_contents($match[1])) === false)
 	$response['error'] = 'Could not fetch lyrics';
 
 else if (preg_match('/((?:&#\d+;|<\\/?i>|<br \\/>){10,})/', $webpage, $match))
