d0a206d9d62eccf002f129562bff2e22864a21d8
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2021-12-27 17:32:42 +0100

    Switch to Genius for lyrics

--- lyrics.php
+++ lyrics.php
@@ -1,16 +1,20 @@
 <?php
 require 'config.php';
-
 date_default_timezone_set('Europe/Amsterdam');
 
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
 function curl_get_contents($url) {
+	$headers = [
+		sprintf('Authorization: Bearer %s', GENIUS_ACCESS_TOKEN)
+	];
+
 	$curl = curl_init($url);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($curl, CURLOPT_HEADER, 0);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 	$out = curl_exec($curl);
 	curl_close($curl);
 	return $out;
@@ -20,29 +24,70 @@ function decode_entities($text) {
 	return preg_replace_callback("/(&#[0-9]+;)/", function($m) { return mb_convert_encoding($m[1], "UTF-8", "HTML-ENTITIES"); }, $text);
 }
 
+function first($items, $test) {
+	foreach ($items as $item)
+		if ($test($item))
+			return $item;
+
+	return null;
+}
+
+function scrape_text($root) {
+	$text = '';
+
+	if ($root->nodeType == XML_ELEMENT_NODE && $root->nodeName == 'br')
+		$text .= "\n";
+
+	foreach ($root->childNodes as $child) {
+		switch ($child->nodeType) {
+			case XML_ELEMENT_NODE:
+				$text .= scrape_text($child);
+				break;
+			case XML_TEXT_NODE:
+				$text .= $child->nodeValue;
+				break;
+		}
+	}
+
+	return $text;
+}
+
+function scrape_lyrics($url) {
+	$html = curl_get_contents($url);
+
+	$dom = new DOMDocument();
+	$dom->loadHTML($html, LIBXML_NOERROR);
+
+	$text = '';
+
+	foreach ($dom->getElementsByTagName('div') as $div) {
+		if (strstr($div->getAttribute('class'), 'Lyrics__Container') !== false)
+			$text .= scrape_text($div) . "\n";
+	}
+
+	return $text;
+}
+
 function main() {
-	$track_search = json_decode(curl_get_contents(sprintf('https://api.happi.dev/v1/music?limit=1&apikey=%s&type=track&q=%s+%s',
-		rawurlencode(HAPPI_API_KEY),
+	$track_search = json_decode(curl_get_contents(sprintf('https://api.genius.com/search?q=%s+%s',
 		rawurlencode($_GET['artist']),
 		rawurlencode($_GET['song']))));
 
-	if (!$track_search->success)
+	if ($track_search->meta->status != 200)
 		return ['error' => 'Could not search for track info'];
 
-	if ($track_search->length < 1)
+	$song = first($track_search->response->hits, function($hit) { return $hit->type == 'song'; });
+
+	if (!$song)
 		return ['error' => 'Could not find track in database'];
 
-	$lyrics_search = json_decode(curl_get_contents(sprintf('https://api.happi.dev/v1/music/artists/%d/albums/%d/tracks/%d/lyrics?apikey=%s',
-		$track_search->result[0]->id_artist,
-		$track_search->result[0]->id_album,
-		$track_search->result[0]->id_track,
-		rawurlencode(HAPPI_API_KEY))));
+	$lyrics = scrape_lyrics($song->result->url);
 
-	if (!$lyrics_search->success)
-		return ['error' => 'Could not fetch lyrics'];
+	if (!$lyrics)
+		return ['error' => 'Could not scrape lyrics'];
 
-	return ['lyrics' => nl2br($lyrics_search->result->lyrics)];
+	return ['lyrics' => nl2br($lyrics)];
 }
 
-header('Content-Type: application/json');
+header('Content-Type: text/plain; charset=utf-8');
 echo json_encode(main());
